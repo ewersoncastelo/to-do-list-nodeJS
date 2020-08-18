03ec90281b9c23a62e2ccee8eefda8e2a17d35ab
Ewerson Castelo <ewerson.castelo@gmail.com>
2020-08-18 06:24:11 -0400

    update gradle and add floating button

--- lib/App.dart
+++ lib/App.dart
@@ -8,6 +8,8 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  int _index = 0;
+
   @override
   Widget build(BuildContext context) {
     return PlatformScaffold(
@@ -35,10 +37,48 @@ class _AppState extends State<App> {
         },
       ),
       material: (context, platform) {
-        return MaterialScaffoldData();
+        return MaterialScaffoldData(
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              print("button tapped");
+            },
+            backgroundColor: Colors.purple,
+            child: Icon(Icons.add),
+            elevation: 6,
+            mini: true,
+          ),
+        );
       },
       body: Center(
         child: Text("Hello"),
+      ),
+      bottomNavBar: PlatformNavBar(
+        currentIndex: _index,
+        itemChanged: (index) {
+          setState(() {
+            _index = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business, color: Colors.black38),
+            title: Text('Screen 1'),
+            activeIcon: Icon(Icons.business, color: Colors.black),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business, color: Colors.black38),
+            title: Text('Screen 2'),
+            activeIcon: Icon(Icons.business, color: Colors.black),
+          ),
+        ],
+        material: (context, platform) {
+          return MaterialNavBarData(
+            type: BottomNavigationBarType.fixed,
+            fixedColor: Colors.white,
+          );
+        },
       ),
     );
   }
