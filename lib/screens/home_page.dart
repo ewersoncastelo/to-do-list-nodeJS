de80b87f495dfa6edf6eebb0f3d6c852d8bbbeea
Ewerson Castelo <ewerson.castelo@gmail.com>
2021-03-28 15:42:06 -0400

    chore: created home page screen

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,78 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:path_provider/path_provider.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List _toDoList = [];
+
+  Future<File> _getFilePath() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_toDoList);
+    final file = await _getFilePath();
+    file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFilePath();
+
+      return file.readAsString();
+    } catch (error) {
+      return null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.fromLTRB(17, 1, 7, 1),
+          child: Form(
+            child: Row(
+              children: [
+                Expanded(
+                  child: PlatformTextField(
+                    material: (context, platform) => MaterialTextFieldData(
+                      decoration: InputDecoration(
+                        labelText: "Nova Tarefa",
+                        labelStyle: TextStyle(color: Colors.blueAccent[100]),
+                      ),
+                    ),
+                    cupertino: (context, platform) => CupertinoTextFieldData(
+                      placeholder: 'Nova Tarefa',
+                      placeholderStyle: TextStyle(
+                        color: Colors.blueAccent[100],
+                      ),
+                    ),
+                  ),
+                ),
+                PlatformButton(
+                  onPressed: () => print('Adicionar'),
+                  child: PlatformText('Adicionar'),
+                  material: (context, platform) => MaterialRaisedButtonData(
+                    color: Colors.blueAccent,
+                    textColor: Colors.white,
+                    elevation: 10,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
