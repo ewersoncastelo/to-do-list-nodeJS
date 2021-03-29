6157b2761219bacd6474892fa1f16916ebd0e8a6
Ewerson Castelo <ewerson.castelo@gmail.com>
2021-03-28 21:31:57 -0400

    chore: created function addTask

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -13,6 +13,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   List _toDoList = [];
 
+  final _toDoController = TextEditingController();
+
   Future<File> _getFilePath() async {
     final directory = await getApplicationDocumentsDirectory();
     return File("${directory.path}/data.json");
@@ -34,42 +36,79 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  void _addTodo() {
+    setState(() {
+      Map<String, dynamic> newTodoTask = Map();
+      newTodoTask["title"] = _toDoController.text;
+      _toDoController.text = "";
+      newTodoTask["ok"] = false;
+      _toDoList.add(newTodoTask);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Container(
           padding: EdgeInsets.fromLTRB(17, 1, 7, 1),
-          child: Form(
-            child: Row(
-              children: [
-                Expanded(
-                  child: PlatformTextField(
-                    material: (context, platform) => MaterialTextFieldData(
-                      decoration: InputDecoration(
-                        labelText: "Nova Tarefa",
-                        labelStyle: TextStyle(color: Colors.blueAccent[100]),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: PlatformTextField(
+                      material: (context, platform) => MaterialTextFieldData(
+                        decoration: InputDecoration(
+                          labelText: "Nova Tarefa",
+                          labelStyle: TextStyle(color: Colors.blueAccent[100]),
+                        ),
                       ),
+                      cupertino: (context, platform) => CupertinoTextFieldData(
+                        placeholder: 'Nova Tarefa',
+                        placeholderStyle: TextStyle(
+                          color: Colors.blueAccent[100],
+                        ),
+                      ),
+                      controller: _toDoController,
                     ),
-                    cupertino: (context, platform) => CupertinoTextFieldData(
-                      placeholder: 'Nova Tarefa',
-                      placeholderStyle: TextStyle(
-                        color: Colors.blueAccent[100],
-                      ),
+                  ),
+                  PlatformButton(
+                    onPressed: _addTodo,
+                    child: PlatformText('Adicionar'),
+                    material: (context, platform) => MaterialRaisedButtonData(
+                      color: Colors.blueAccent,
+                      textColor: Colors.white,
+                      elevation: 10,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            padding: EdgeInsets.only(top: 10),
+            itemCount: _toDoList.length,
+            itemBuilder: (context, index) {
+              return Material(
+                child: CheckboxListTile(
+                  onChanged: (completedTask) {
+                    setState(() {
+                      _toDoList[index]["ok"] = completedTask;
+                    });
+                  },
+                  title: Text(_toDoList[index]["title"]),
+                  value: _toDoList[index]["ok"],
+                  secondary: CircleAvatar(
+                    child: Icon(
+                      _toDoList[index]["ok"] ? Icons.check : Icons.error,
                     ),
                   ),
                 ),
-                PlatformButton(
-                  onPressed: () => print('Adicionar'),
-                  child: PlatformText('Adicionar'),
-                  material: (context, platform) => MaterialRaisedButtonData(
-                    color: Colors.blueAccent,
-                    textColor: Colors.white,
-                    elevation: 10,
-                  ),
-                ),
-              ],
-            ),
+              );
+            },
           ),
         ),
       ],
