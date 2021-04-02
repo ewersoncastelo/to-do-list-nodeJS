bfb197d9f161a67aa43d8293754fdb5798c2773c
Ewerson Castelo <ewerson.castelo@gmail.com>
2021-04-02 07:00:12 -0400

    chore: removed nestedscroll for cupertino

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -19,51 +17,20 @@ class TodoList extends StatefulWidget {
 class _TodoListState extends State<TodoList> {
   @override
   Widget build(BuildContext context) {
-    return Platform.isAndroid
-        ? MaterialApp(
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-              primaryColor: Colors.blue[900],
-              brightness: Brightness.light,
-            ),
-            home: Scaffold(
-              appBar: AppBar(
-                title: Text("Lista de Tarefas"),
-                centerTitle: false,
-                brightness: Brightness.dark,
-              ),
-              body: HomePage(),
-            ),
-          )
-        : CupertinoApp(
-            debugShowCheckedModeBanner: false,
-            theme: CupertinoThemeData(
-              barBackgroundColor: Colors.blue[900],
-              textTheme: CupertinoTextThemeData(
-                navTitleTextStyle: TextStyle(
-                  color: CupertinoColors.white,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              brightness: Brightness.light,
-            ),
-            home: CupertinoPageScaffold(
-              child: NestedScrollView(
-                headerSliverBuilder: (context, innerBoxIsScrolled) {
-                  return <Widget>[
-                    CupertinoSliverNavigationBar(
-                      largeTitle: Text(
-                        "Lista de Tarefas",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      brightness: Brightness.dark,
-                    ),
-                  ];
-                },
-                body: HomePage(),
-              ),
-            ),
-          );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.blue[900],
+        brightness: Brightness.light,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Lista de Tarefas"),
+          centerTitle: false,
+          brightness: Brightness.dark,
+        ),
+        body: HomePage(),
+      ),
+    );
   }
 }
